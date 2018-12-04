bfaf11925139ea2de19d10c6ca1bdbbe3f78c6f2
JMD <josephm.diza@gmail.com>
2018-12-03 22:23:54 -0800

    Finishing up

--- PHP/login.php
+++ PHP/login.php
@@ -1,8 +1,13 @@
 <?php
 //include '/php/getUserInfo.php';
 // Make sure to create an entry in the SQL table for the login
-$account_num = $_POST['account_num'];
-$password = $_POST['password'];
+
+// $encryptedData = $_POST['request'];
+// $encryptedKey = $_POST['key'];
+
+require ("encrypt.php");
+
+$encryption = new Encryption;
 
 $con = new mysqli("localhost", "id6011112_joseph", "Password", "id6011112_atm");
 
@@ -10,10 +15,38 @@ if ($con->connect_error)
   {
     die("Connection failed: " . $conn->connect_error);
   }
- 
-  $query = "SELECT * FROM USERS WHERE acct_number LIKE '$account_num' AND password LIKE '$password'";
+
+
+
+// $query = "SELECT * FROM ENCRYPTION";
+
+// $account_num = $_POST['account_num'];
+
+/*
+$account_num = $_POST['account_num'];
+$password = $_POST['password'];
+$secretKey = $_POST['secretKey'];
+$iv = $_POST['iv'];
+
+
+$encryption->setKey($secretKey);
+$account_num = $encryption->decrypt($account_num, $iv);
+$password = $encryption->decrypt($password, $iv);
+*/
+
+$secretKey = $_POST['secretKey'];
+$iv = $_POST['iv'];
+$account_num = $encryption->decrypt($_POST['account_num'], $iv);
+$password = $encryption->decrypt($_POST['password'], $iv);
+
+// $encryption->setKey($secretKey);
+
+$sql = "SELECT * FROM USERS WHERE acct_number LIKE '$account_num' AND password LIKE '$password'";
+// $result = $con->query($sql);
+// $row = mysqli_fetch_assoc($result);
+// print (json_encode($row)); Never print this out.
   
-  $sql = mysqli_query($con, $query);
+
 if(mysqli_num_rows($sql) > 0)
   {
       // if a row was found with these credentials â€”> login successful
