9b3c116306bf66376fcf6920213bf9b4f632f9c9
Joseph Diza <josephm.diza@gmail.com>
2020-10-22 19:08:47 -0700

    Created scripts to set $CLASSPATH

---
+++ setcp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#export CLASSPATH=$HOME/workspace/apps/BankApp/lib/*:lib
+#export CLASSPATH=".:.*"
+JUNIT_HOME=./lib/junit4.10
+export CLASSPATH=.:./*:$HOME/workspace/apps/BankApp/lib/*:$JUNIT_HOME/*:./bin/*:./bin/crypt/*
