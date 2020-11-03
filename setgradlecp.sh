f59dc045c6f52cf292032b0887f5446f36d0c1df
Joseph Diza <josephm.diza@gmail.com>
2020-11-02 21:08:59 -0800

    Add current dir to classpath script

--- setgradlecp.sh
+++ setgradlecp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-GRADLE_CLASSPATH="cat ./syntastic_javac_config | cut -d " " -f 3"
-export CLASSPATH=$GRADLE_CLASSPATH 
+GRADLE_CLASSPATH=$(cat .syntastic_javac_config | cut -d " " -f 4 | tr -d '"')
+export CLASSPATH=./:$GRADLE_CLASSPATH 
