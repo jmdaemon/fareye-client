efef519c46065bfede70711cef9ac34a45ae6973
Joseph Diza <josephm.diza@gmail.com>
2020-11-09 12:45:11 -0800

    Remove redundant import in CryptUtilsTests

--- src/test/java/encryption/CryptUtilsTests.java
+++ src/test/java/encryption/CryptUtilsTests.java
@@ -3,7 +3,6 @@ import crypt.utils.*;
 
 //import static org.junit.jupiter.api.Assertions.assertEquals; 
 import static org.junit.jupiter.api.Assertions.*; 
-import static org.junit.jupiter.api.Assertions.*; 
 
 import org.junit.jupiter.api.Test;
 
