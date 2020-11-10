fca1bb7fbe2fe0b17387a108c680fcfe1c1be23a
Joseph Diza <josephm.diza@gmail.com>
2020-11-09 17:56:04 -0800

    Add withdraw method unit tests for BankAccount

--- src/test/java/bankaccount/BankAccountTests.java
+++ src/test/java/bankaccount/BankAccountTests.java
@@ -27,7 +27,7 @@ public class BankAccountTests {
     String lName = newAcct.getLName();
 
     assertNotNull(acctNum, "Account Number is initialized");
-    assertEquals(balance, 0, "User has no money in account");
+    assertEquals(0, balance, "User has no money in account");
     assertNotNull(fName, "User's first name is: " + fName);
     assertNotNull(lName, "User's last name is: " + lName);
   }
@@ -36,18 +36,20 @@ public class BankAccountTests {
   public void Deposit_NegativeAmount_ReturnsFalse() {
     boolean res = bankAccount.deposit(-1000);
     String log = bankAccount.getLog().toString();
-    assertEquals(res, false, "Cannot deposit a negative amount"); 
     String logRes = parseLog(log, 3); // Cut timeStamp from log
+    assertEquals(false, res, "Cannot deposit a negative amount"); 
     assertEquals("Deposit Unsuccessful", logRes, "Account Log has recorded the failed deposit");
   }
 
   @Test
   public void Deposit_AmountOverflow_ExceptionThrown() {
-    boolean res = bankAccount.deposit(Double.MAX_VALUE);
+    boolean res = bankAccount.deposit(Double.MAX_VALUE); // Win the Lottery
+
     // TODO: Finish buffer overflow test
     // Need to check if amount is greater than MAX_VALUE?
     // Should this throw an Exception?
     // Or should we use BigDouble
+    // Ignore for now
 
     double bal = bankAccount.getBalance();
     String log = bankAccount.getLog().toString();
@@ -58,16 +60,18 @@ public class BankAccountTests {
   public void Deposit_1000_ReturnsTrue() {
     boolean res = bankAccount.deposit(1000);
     String log = bankAccount.getLog().toString();
-    System.out.println("Log" + log);
     String logRes = parseLog(log, 3);
-
-    System.out.println("logRes: " + logRes);
-    assertEquals(res, true, "Deposit of $1000 is successful");
-    assertEquals("Deposit Successful", logRes, "Account Log has successfuly recorded the deposit");
+    assertEquals(true, res, "Deposit of $1000 is successful");
+    assertEquals("Deposit Successful", logRes, "Account Log has successfully recorded the deposit");
   }
 
   @Test
-  public void Withdraw_NegativeAmount_ExceptionThrown() {
+  public void Withdraw_NegativeAmount_ReturnsFalse() {
+    boolean res = bankAccount.withdraw(-1000);
+    String log = bankAccount.getLog().toString();
+    String logRes = parseLog(log, 3);
+    assertEquals(false, res, "Cannot withdraw a negative amount");
+    assertEquals("Withdrawal Unsuccessful", logRes,"Account Log has recorded the failed withdrawal");
 
   }
 
@@ -78,6 +82,12 @@ public class BankAccountTests {
 
   @Test
   public void Withdraw_1000_True() {
+    bankAccount.deposit(1000); // Give our mock a starting balance
+    boolean res = bankAccount.withdraw(1000);
+    String log = bankAccount.getLog().toString();
+    String logRes = parseLog(log, 5); // 3 is the deposit msg from earlier
+    assertEquals(true, res, "Withdrawal of $1000 is successful");
+    assertEquals("Withdrawal Successful", logRes,"Account Log has successfully recorded the withdrawal");
 
   }
 
