bc29fdcccc0759717c66c88bfc6b098d28405784
Joseph Diza <josephm.diza@gmail.com>
2020-11-16 10:37:49 -0800

    Start feature AES-encryption

--- src/test/java/encryption/CryptUtilsTests.java
+++ src/test/java/encryption/CryptUtilsTests.java
@@ -5,75 +5,78 @@ import app.crypt;
 public class CryptUtilsTests {
 
   public void encrypt_(){
+    CryptUtils cutil =  new CryptUtils();
+    String res = cutil.encrypt("This is the plaintext");
+    assertNotEquals("This is the plaintext", res, "Ciphertext should not equal plaintext");
   }
 
   public void decrypt_(){
   }
 
-  public void encrypt_Salt(){
-  }
+  //public void encrypt_Salt(){
+  //}
 
-  public void decrypt_Salt_(){
-  }
+  //public void decrypt_Salt_(){
+  //}
 
-  public void encrypt_IV_(){
-  }
+  //public void encrypt_IV_(){
+  //}
 
-  public void decryptIV_(){
-  }
+  //public void decryptIV_(){
+  //}
 
-  public void encrypt_IVSalt_(){
-  }
+  //public void encrypt_IVSalt_(){
+  //}
 
-  public void decryptIVSALT_(){
-  }
+  //public void decryptIVSALT_(){
+  //}
 
-  public void encodeBase64_(){
-  }
+  //public void encodeBase64_(){
+  //}
 
-  public void decodeBase64_(){
-  }
+  //public void decodeBase64_(){
+  //}
 
-  public void encodeBase64_Salt(){
-  }
+  //public void encodeBase64_Salt(){
+  //}
 
-  public void decodeBase64_Salt(){
-  }
+  //public void decodeBase64_Salt(){
+  //}
 
-  public void encodeBase64_IV(){
-  }
+  //public void encodeBase64_IV(){
+  //}
 
-  public void decodeBase64_IV(){
-  }
+  //public void decodeBase64_IV(){
+  //}
 
-  public void encodeBase64_IVSalt(){
-  }
+  //public void encodeBase64_IVSalt(){
+  //}
 
-  public void decodeBase64_IVSalt(){
-  }
+  //public void decodeBase64_IVSalt(){
+  //}
 
-  public void getKey_() {
-  }
+  //public void getKey_() {
+  //}
 
-  public void getSalt_() {
-  }
+  //public void getSalt_() {
+  //}
   
-  public void getKey_FromKS() {
-  }
+  //public void getKey_FromKS() {
+  //}
 
-  public void getSalt_FromKS() {
-  }
+  //public void getSalt_FromKS() {
+  //}
 
-  public void genKey_AES() {
-  }
+  //public void genKey_AES() {
+  //}
 
-  public void genKey_RSA() {
-  }
+  //public void genKey_RSA() {
+  //}
 
-  public void genSalt() {
-  }
+  //public void genSalt() {
+  //}
 
-  public void genIV() {
-  }
+  //public void genIV() {
+  //}
 
 }
