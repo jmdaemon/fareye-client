01bdfaebda3393b9ebdbcf22db0f8780b2f0f9ef
Joseph Diza <josephm.diza@gmail.com>
2020-11-18 21:23:46 -0800

    Remove dead imports, pull encrypt into AES, RSA

--- src/main/java/encryption/CryptUtils.java
+++ src/main/java/encryption/CryptUtils.java
@@ -2,18 +2,12 @@ package app.crypt.utils;
 
 import java.util.Arrays;
 
-import java.security.KeyPairGenerator;
-import java.security.KeyPair;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class CryptUtils {
   private String plaintext;
   private final int IV_LENGTH = 12;
   private final int SALT_LENGTH = 16;
-
 
   public CryptUtils() {
     this.plaintext = null;
@@ -31,10 +25,6 @@ public class CryptUtils {
 
   public byte[] genSalt() {
     return genRandomBytes(SALT_LENGTH);
-  }
-
-  public String encrypt(String plaintext) { 
-    return "This is not the plaintext";
   }
 
 }
