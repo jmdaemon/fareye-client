fc6ba4aac270891686983b7cf252961fee8ba24f
Joseph Diza <josephm.diza@gmail.com>
2020-11-18 21:49:02 -0800

    Add UTF-8 charset variable to CryptUtils

--- src/main/java/encryption/CryptUtils.java
+++ src/main/java/encryption/CryptUtils.java
@@ -3,11 +3,15 @@ package app.crypt.utils;
 import java.util.Arrays;
 
 import java.security.SecureRandom;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class CryptUtils {
   private String plaintext;
   private final int IV_LENGTH = 12;
   private final int SALT_LENGTH = 16;
+
+  public static final Charset UTF_8 = StandardCharsets.UTF_8;
 
   public CryptUtils() {
     this.plaintext = null;
