43fe76f425ad4b7ba4439e15d73c47a1920b143f
Joseph Diza <josephm.diza@gmail.com>
2020-11-20 14:11:24 -0800

    Implement genKeyPswd for AES cipher

--- src/main/java/encryption/AESCipher.java
+++ src/main/java/encryption/AESCipher.java
@@ -4,17 +4,25 @@ import app.crypt.utils.*;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.KeyGenerator;
 import java.security.SecureRandom;
 import javax.crypto.SecretKey;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
 
 public class AESCipher extends CryptUtils {
 
   private static final String ALGORITHM = "AES/GCM/NoPadding";
   private static final int TAG_LENGTH_BIT = 128;
   private final int AES_KEY_LENGTH = 256;
+
+  private final int ITERATION_COUNT = 65536;
 
   public AESCipher() {
 
@@ -32,4 +40,18 @@ public class AESCipher extends CryptUtils {
     byte[] result = cipher.doFinal(plaintext);
     return result;
   }
+
+  public SecretKey genKeyPswd(String pswd, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+    KeySpec spec = new PBEKeySpec(pswd.toCharArray(), salt, ITERATION_COUNT, AES_KEY_LENGTH);
+    SecretKey result = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+    return result;
+  }
+
+  //public byte[] encrypt(byte[] plaintext, SecretKey key, byte[] iv, byte[] salt) throws Exception { 
+    //Cipher cipher = Cipher.getInstance(ENCRYPT_ALGO);
+    //cipher.init(Cipher.ENCRYPT_MODE, aesKeyFromPassword, new GCMParameterSpec(TAG_LENGTH_BIT, iv));
+
+  //}
+  
 }
--- src/test/java/encryption/AESCipherTests.java
+++ src/test/java/encryption/AESCipherTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import javax.crypto.SecretKey;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 public class AESCipherTests {
   private AESCipher cipher = new AESCipher();
@@ -16,6 +17,11 @@ public class AESCipherTests {
   public void genKey_AES_ReturnAESKey() throws NoSuchAlgorithmException {
     assertNotNull(cipher.genKey(), "AES Key should be initialized");
   }
+
+  @Test
+  public void genKeyPswd_AES_ReturnAESKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
+    assertNotNull(cipher.genKeyPswd("This is the user password", cipher.genSalt()), "AES Key should be initialized");
+  }
   
   @Test
   public void encrypt_Plaintext_ReturnAESCiphertext() throws Exception {
@@ -23,4 +29,28 @@ public class AESCipherTests {
     assertNotNull(res, "Ciphertext should be initialized");
     assertNotEquals("This is the plaintext", new String (res, cipher.UTF_8), "Ciphertext should not equal plaintext");
   }
+
+  //public void encrypt_SaltPlaintext_ReturnAESCiphertext(){
+    //byte[] res = cipher.encryptSalt( "This is the plaintext".getBytes(cipher.UTF_8), cipher.genKey(), cipher.genIV() );
+    //byte[] salt = cipher.getSalt();
+    //assertNotNull(res, "Ciphertext should be initialized");
+    //assertNotEquals("This is the plaintext", new String (res, cipher.UTF_8), "Ciphertext should not equal plaintext");
+    //assertNotNull(salt, "Salt should be initialized");
+  //}
+
+  //public void decrypt_Salt_(){
+  //}
+
+  //public void encrypt_IV_(){
+  //}
+
+  //public void decryptIV_(){
+  //}
+
+  //public void encrypt_IVSalt_(){
+  //}
+
+  //public void decryptIVSALT_(){
+  //}
+
 }
