61e9f0de6d9dada47a8e69f51914fb4731e16611
Joseph Diza <josephm.diza@gmail.com>
2020-11-27 12:49:30 -0800

    Rename search -> searchFor unit test, add test stubs

--- src/test/java/bankaccount/LogTests.java
+++ src/test/java/bankaccount/LogTests.java
@@ -9,8 +9,35 @@ import org.junit.jupiter.api.Test;
 public class LogTests {
   private BankAccount acct = new BankAccount();
 
+  //@Test logAppend() {
+  //}
+
+  //@Test logTo() {
+  //}
+
+  //@Test logMessage_Msg() {
+  //}
+
+  //@Test logMessage_MsgAmt() {
+  //}
+
+  //@Test logMessage_AcctsAmt() {
+  //}
+
+  //@Test logMessage_AcctAmt() {
+  //}
+
+  //@Test public search() {
+  //}
+
+  //@Test public parseLog_NewlineDelim() {
+  //}
+
+  //@Test public parseLog_TabDelim() {
+  //}
+
   @Test
-  public void search_ReturnsLogMsg() {
+  public void searchFor_ReturnsLogMsg() {
     acct.deposit(100);
     String res = acct.getLog().searchFor("Deposit Successful");
     assertNotNull(res, "Deposit should be logged in the log");
