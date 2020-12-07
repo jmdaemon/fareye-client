45432efcf38adad253ea9f1589a2685bb3d2ec7a
Joseph Diza <josephm.diza@gmail.com>
2020-12-06 22:41:12 -0800

    Remove dead code in Log unit tests

--- src/test/java/bankaccount/LogTests.java
+++ src/test/java/bankaccount/LogTests.java
@@ -9,11 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.*; 
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.FileWriter;
-import java.lang.StringBuffer;
-
 public class LogTests {
   private static final String filepath = "./transaction_history.csv";
   private Log log;
@@ -27,13 +22,6 @@ public class LogTests {
     this.acct = new BankAccount();
     this.targ = new BankAccount();
   }
-
-  //@BeforeEach
-  //public void setUpLog() throws IOException { 
-    //String logEntry1 = (genTimeStamp() + ", " + "New Bank Account Created, ");
-    //String logEntry2 = (genTimeStamp() + ", " + "Deposit Successful [$100.0], " );
-    //initializeFile(logEntry1, logEntry2);
-  //}
 
   @Test 
   public void logAppend_ShouldLogMessage() { 
@@ -58,11 +46,6 @@ public class LogTests {
   @Test 
   public void logMessage_MsgAmt_ShouldLogMsgWithAmt() {
     log.logMessage("logMessage with amount", 100);
-    //String resLog = searchLog("logMessage with amount");
-    //String resAmt = searchLog("\\[\\$100.0\\]");
-    //assertEquals("logMessage with amount", resLog);
-    //assertEquals("[$100.0]", resAmt);
-    //String res = searchLog("logMessage with amount" + "\\[\\$100.0\\]");
     assertEquals("logMessage with amount  [$100.0]", searchLog("logMessage with amount"));
   }
 
@@ -78,54 +61,7 @@ public class LogTests {
   @Test 
   public void logMessage_FailedAcctAmt_ShouldLogFailedTransfer() {
     acct.getLog().logMessage(acct, 100);
-    //String acctResLog = searchLog("Transfer Failed");
-    //String acctResAmt = searchLog("\\[\\$100.0 to account \\d{1,5}\\]");
     String res = searchLog("Transfer Failed " + "\\[\\$100.0 to account \\d{1,5}\\]");
-    //assertEquals("Transfer Failed " +, acctResLog);
     assertEquals("Transfer Failed [$100.0 to account " + acct.getAcctNum() + "]", res);
   }
-
-  //@Test 
-  //public void parseLog_NewlineDelim_ShouldReturnLogEntry() {
-    //log.logMessage("Testing parseLog \nwith newline delimeter\n");
-    //String res = log.parseLog(log.search("Testing parseLog"), "\\r?\\n");
-    //assertEquals("Testing parseLog", res);
-  //}
-
-  //@Test 
-  //public void parseLog_TabDelim_ShouldReturnLogEntry() {
-    //log.logMessage("Testing parseLog \twith tab delimeter\t\n");
-    //String res = log.parseLog(log.search("Testing parseLog"), "\\t+");
-    //assertEquals("Testing parseLog", res);
-  //}
-
-  //@Test
-  //public void searchLog_ReturnsLogMsg() {
-    //acct.deposit(100);
-    //String res = searchLog("Deposit Successful");
-    //assertEquals("Deposit Successful", res);
-  //}
-
-  /** Create setup code for creating mock ./transaction_history.csv */
-
-  //@Test
-  //public void fileExists_ReturnsTrue() {
-    //assertEquals(true, acct.getLog().fileExists(filepath));
-    //assertEquals(false, acct.getLog().fileExists("imaginaryfile.csv"));
-  //}
-
-  //@Test
-  //public void writeToFile_CreatesCSVFile() {
-    //acct.deposit(100);
-    //acct.writeToFile("./transaction_history.csv");
-    //assertEquals(true, acct.getLog().fileExists(filepath));
-  //}
-
-  //@Test
-  //public void readFile_transaction_history_ReturnsTrue() {
-    //String logCSV = acct.getLog().readFile(filepath);
-    //System.out.println(logCSV);
-    //String res = acct.getLog().searchFileFor("Deposit Successful", logCSV);
-    //assertEquals("Deposit Successful", res);
-  //}
 }
