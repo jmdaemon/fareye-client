afbf6c72dea446ee6f47111dba0e2314a71c17c1
Joseph Diza <josephm.diza@gmail.com>
2020-12-12 17:20:27 -0800

    Create abstract class Connection

---
+++ src/main/java/connect/Connection.java
@@ -0,0 +1,27 @@
+package app.connect;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public abstract class Connection {
+  //protected String url;
+
+  URL formRequest(String site) {
+    URL url = null;
+    try {
+      url = new URL(site);
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+    return url;
+  }
+
+  public abstract String get(String to, String params) throws IOException;
+  public abstract String post(String to, String params) throws IOException;
+}
