e919075cb1ab09654568e8b15ed67d23e8410c10
Joseph Diza <josephm.diza@gmail.com>
2020-12-13 14:37:43 -0800

    Add controller for LoginView

--- src/main/java/interface/controller/LoginController.java
+++ src/main/java/interface/controller/LoginController.java
@@ -1,7 +1,43 @@
-package app.view;
+package app.ui;
 
-import javafx.scene.layout.HBox;
+//import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+//import javafx.scene.Scene;
+//import javafx.scene.Stage;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-public class LoginView {
+
+public class LoginController {
+
+  @FXML
+  private Button enter;
+
+  @FXML
+  private Label userName_Label;
+
+  @FXML
+  private TextField userName;
+
+  @FXML
+  private Label password_Label;
+
+  @FXML
+  private PasswordField password;
+  
+  @FXML
+  private Hyperlink resetPassword;
+
+  public String getUserName() {
+    return userName.getText();
+  }
   
 }
