c18f0c30e21fa2e2314f82f99d7d7161c22f272e
Joseph Diza <josephm.diza@gmail.com>
2020-12-13 16:20:45 -0800

    Remove dead imports and member vars in LoginController

--- src/main/java/interface/controller/LoginController.java
+++ src/main/java/interface/controller/LoginController.java
@@ -1,20 +1,11 @@
 package app.ui;
 
-//import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-//import javafx.scene.Scene;
-//import javafx.scene.Stage;
-import javafx.scene.layout.GridPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
 
 public class LoginController {
 
@@ -22,13 +13,7 @@ public class LoginController {
   private Button enter;
 
   @FXML
-  private Label userName_Label;
-
-  @FXML
   private TextField userName;
-
-  @FXML
-  private Label password_Label;
 
   @FXML
   private PasswordField password;
@@ -38,6 +23,10 @@ public class LoginController {
 
   public String getUserName() {
     return userName.getText();
+  }
+
+  public String getPassword() {
+    return password.getText();
   }
   
 }
