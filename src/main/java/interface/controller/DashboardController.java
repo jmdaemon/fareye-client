7630d8b4050d9c564d36dde73fa1c2c52d244867
Joseph Diza <josephm.diza@gmail.com>
2020-12-14 20:53:38 -0800

    Add DashboardController

--- src/main/java/interface/controller/DashboardController.java
+++ src/main/java/interface/controller/DashboardController.java
@@ -1,5 +1,22 @@
 package app.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class DashboardController {
 
+    @FXML
+    private Label date;
+
+    @FXML
+    private Label acct_number;
+
+    @FXML
+    private Label balance;
+
+    @FXML
+    private Label acct_balance;
+
 }
+
