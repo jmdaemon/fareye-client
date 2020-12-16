5860166b513b3c85c376e8ff24a25c12f26524ca
Joseph Diza <josephm.diza@gmail.com>
2020-12-15 21:37:17 -0800

    Remove calls to scenicView imports and show

--- src/main/java/interface/UI.java
+++ src/main/java/interface/UI.java
@@ -14,15 +14,15 @@ public class UI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
       primaryStage.setTitle("My First JavaFX App");
-      FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxmls/DashboardView.fxml"));
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxmls/LoginView.fxml"));
+      //FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxmls/DashboardView.fxml"));
       GridPane root = loader.load();
-      root.getStylesheets().add(getClass().getResource("/resources/assets/Dashboard.css").toExternalForm());
+      //root.getStylesheets().add(getClass().getResource("/resources/assets/Dashboard.css").toExternalForm());
 
-      Scene scene = new Scene(root, 600, 400);
+      Scene scene = new Scene(root, 600, 400); 
 
       primaryStage.setScene(scene);
       primaryStage.show();
-      //ScenicView.show(scene);
     }
 
     public static void main(String[] args) {
