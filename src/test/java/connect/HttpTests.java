e2782f9323e118106d60428912cc672630151d65
Joseph Diza <josephm.diza@gmail.com>
2020-12-21 17:33:08 -0800

    Replace Http pings with mock server expectations

--- src/test/java/connect/HttpTests.java
+++ src/test/java/connect/HttpTests.java
@@ -1,39 +1,79 @@
 package app.connect;
 
 import static org.junit.jupiter.api.Assertions.*; 
+import org.junit.jupiter.api.extension.*;
 import org.junit.jupiter.api.*;
 
 import java.util.HashMap;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.mockserver.junit.jupiter.*;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.integration.ClientAndServer;
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+import static org.mockserver.model.Parameter.param;
+import static org.mockserver.matchers.Times.exactly;
 
 public class HttpTests {
+
+  private static ClientAndServer mockServer;
+
+  private static final String mockServerURL = "localhost";
   private static final String getFrom = "http://webcode.me";
   private static final String postTo  = "https://httpbin.org/post";
   private HashMap<String, String> values;
 
-  @BeforeEach
-  private void setUp() { 
-    this.values = new HashMap<String, String>() {{ 
-      put("name", "John Doe"); 
-      put ("occupation", "gardener"); 
-    }};
+  @BeforeAll
+  public static void startMockServer() {
+      mockServer = startClientAndServer(1080);
+  }
+
+  @AfterAll
+  public static void stopMockServer() {
+      mockServer.stop();
+  } 
+
+  //@BeforeEach
+  //private void setUp() { 
+    //this.values = new HashMap<String, String>() {{ 
+      //put("name", "John Doe"); 
+      //put ("occupation", "gardener"); 
+    //}};
+  //}
+
+  public void createPingExpectation() {
+    new ClientAndServer("localhost", 1080)
+      .when(
+          request()
+          .withMethod("GET"),
+          //.withPath("/index.html"),
+          exactly(1))
+      .respond(
+          response()
+          .withStatusCode(200)
+          .withBody("Successfully pinged server")
+          );
   }
 
   @Test
   public void sendGET_ToSite_ReturnResponse() throws Exception {
     Http conn = new Http();
-    String response = conn.get(getFrom);
+    createPingExpectation();
+    //String response = conn.get(getFrom);
+    //String response = conn.get(mockServerURL + ":1080");
+    String response = conn.get("http://127.0.0.1:1080");
     assertNotNull(response);
   }
 
-  @Test
-  public void sendPOST_ToSite_ReturnResponse() throws Exception {
-    Http conn = new Http(); 
-    var objectMapper = new ObjectMapper(); 
-    String requestBody = objectMapper.writeValueAsString(values);
-    String response = conn.post(postTo, requestBody);
-    assertNotNull(response);
-  }
+  //@Test
+  //public void sendPOST_ToSite_ReturnResponse() throws Exception {
+    //Http conn = new Http(); 
+    //var objectMapper = new ObjectMapper(); 
+    //String requestBody = objectMapper.writeValueAsString(values);
+    //String response = conn.post(postTo, requestBody);
+    //assertNotNull(response);
+  //}
   
 }
--- src/test/java/connect/HttpsTests.java
+++ src/test/java/connect/HttpsTests.java
@@ -3,41 +3,71 @@ package app.connect;
 import static org.junit.jupiter.api.Assertions.*; 
 import org.junit.jupiter.api.*;
 
+import org.mockserver.integration.ClientAndServer;
+
 public class HttpsTests { 
+  private static ClientAndServer mockServer;
   private static final String getFrom = "http://webcode.me";
   private static final String postTo  = "https://httpbin.org/post";
 
-  @BeforeEach
-  private void setUp() { 
-    this.values = new HashMap<String, String>() {{ 
-      put("name", "John Doe"); 
-      put ("occupation", "gardener"); 
-    }};
-  }
+  //@BeforeAll
+  //public void startServer() { 
+    //mockServer = new MockServerClient("localhost", 1080) 
+      //.when( 
+          //request() 
+          //.withMethod("POST") 
+          //.withPath("/login")
+          //.withBody("{username: 'foo', password: 'bar'}")
+          //) 
+      //.respond( 
+          //response() 
+          //.withStatusCode(302) 
+          //.withCookie( 
+            //"sessionId", "2By8LOhBmaW5nZXJwcmludCIlMDAzMW"
+            //) 
+          //.withHeader( 
+            //"Location", "https://www.mock-server.com"
+            //)
+          //);
+      ////mockServer = startClientAndServer(1080);
+  //} 
 
-  @Test
-  public void ping_Google_ReturnsResult() {
-    Https conn = new Https();
-    String result = conn.ping("https://google.com");
-    assertNotNull(result);
-  }
+  //@AfterAll
+  //public static void stopMockServer() {
+      //mockServer.stop();
+  //} 
 
-  @Test
-  public void sendGET_ToSite_ReturnResponse() throws Exception {
-    Https conn = new Https();
-    String response = conn.get(getFrom);
-    System.out.println(response);
-    assertNotNull(response);
-  }
+  //@BeforeEach
+  //private void setUp() { 
+    //this.values = new HashMap<String, String>() {{ 
+      //put("name", "John Doe"); 
+      //put ("occupation", "gardener"); 
+    //}};
+  //}
 
-  @Test
-  public void sendPOST_ToSite_ReturnResponse() throws Exception {
-    Https conn = new Https(); 
-    var objectMapper = new ObjectMapper(); 
-    String requestBody = objectMapper.writeValueAsString(values);
-    String response = conn.post(postTo, requestBody);
+  //@Test
+  //public void ping_Google_ReturnsResult() {
+    //Https conn = new Https();
+    //String result = conn.ping("https://google.com");
+    //assertNotNull(result);
+  //}
+
+  //@Test
+  //public void sendGET_ToSite_ReturnResponse() throws Exception {
+    //Https conn = new Https();
+    //String response = conn.get(getFrom);
     //System.out.println(response);
-    assertNotNull(response);
-  }
+    //assertNotNull(response);
+  //}
+
+  //@Test
+  //public void sendPOST_ToSite_ReturnResponse() throws Exception {
+    //Https conn = new Https(); 
+    //var objectMapper = new ObjectMapper(); 
+    //String requestBody = objectMapper.writeValueAsString(values);
+    //String response = conn.post(postTo, requestBody);
+    ////System.out.println(response);
+    //assertNotNull(response);
+  //}
 
 }
