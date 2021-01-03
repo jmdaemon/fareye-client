2ea495d58c350780cfd4ddef4968dfcf9d5456b0
Joseph Diza <josephm.diza@gmail.com>
2021-01-02 16:26:03 -0800

    Create DepositView and Controller

--- src/main/java/interface/controller/DashboardController.java
+++ src/main/java/interface/controller/DashboardController.java
@@ -2,16 +2,25 @@ package app.ui;
 
 import app.bankAccount.*;
 
+
+
 import java.util.ResourceBundle;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.io.IOException;
 
-import javafx.application.Platform;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.application.Platform;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.input.InputEvent;
 
 public class DashboardController implements Initializable {
   private BankAccount user;
@@ -44,10 +53,29 @@ public class DashboardController implements Initializable {
   @FXML
   private Label acct_balance;
 
+  @FXML
+  private Button deposit;
+
   public void setUser(BankAccount user) {
     this.user = user;
     acct_number.setText("Account " + String.valueOf(this.user.getAcctNum()));
     acct_balance.setText(this.user.getBalance().toString());
+  }
+
+  public void loadDepositView(InputEvent event) {
+    Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxmls/DepositView.fxml"));
+    GridPane root = null;
+    try { 
+      root = loader.load();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    //root.getStylesheets().add(getClass().getResource("/resources/assets/DepositView.css").toExternalForm());
+    Scene scene = new Scene(root, 600, 400); 
+    stage.setScene(scene);
+
   }
 
   @Override
---
+++ src/main/java/interface/controller/DepositController.java
@@ -0,0 +1,30 @@
+package app.ui;
+
+import java.util.ResourceBundle;
+import java.net.URL;
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+public class DepositController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TextField depositAmount;
+
+    @FXML
+    private Label depositAmtLabel;
+
+    @FXML
+    private Button depositButton;
+
+    @FXML
+    private Label currency;
+
+}
