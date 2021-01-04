bf6f0881d3a77033bece45a1c417937d019bf6ea
Joseph Diza <josephm.diza@gmail.com>
2021-01-03 19:19:47 -0800

    Remove dead code in DashboardController

--- src/main/java/interface/controller/DashboardController.java
+++ src/main/java/interface/controller/DashboardController.java
@@ -2,8 +2,6 @@ package app.ui;
 
 import app.bankAccount.*;
 
-import java.util.ResourceBundle;
-//import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.io.IOException;
@@ -11,7 +9,7 @@ import java.io.IOException;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-//import javafx.fxml.Initializable;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.application.Platform;
@@ -20,10 +18,6 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.input.InputEvent;
 
-//import org.kordamp.ikonli.javafx.IkonResolver.*;
-//import org.kordamp.ikonli.*;
-//import org.kordamp.ikonli.javafx.FontIcon.*;
-//import org.kordamp.ikonli.fontawesome5.*;
 import org.kordamp.ikonli.*;
 import org.kordamp.ikonli.javafx.*;
 import org.kordamp.ikonli.fontawesome5.*;
@@ -34,68 +28,23 @@ public class DashboardController {
   @FXML
   private OverlayController overlayController;
 
-  //private Thread timer = new Thread(() -> {
-    //Platform.runLater(()-> {
-        //this.date.setText(getDateAndTime()); 
-    //});
-  //}); 
-
-  //private String getDateAndTime() {
-    //SimpleDateFormat date = new SimpleDateFormat("EEEE h:mm a");
-    //return date.format(new Date());
-  //}
-
-  @FXML
-  private Label date;
-
-  @FXML
-  private Label acct_number;
-
-  @FXML
-  private Label balance;
-
-  @FXML
-  private Label acct_balance;
-
-  @FXML
-  private Button deposit;
-
   public void setUser(BankAccount user) {
     this.user = user;
-    ////acct_number.setText("Account " + String.valueOf(this.user.getAcctNum()));
     overlayController.setUser(user);
-    ////acct_balance.setText(this.user.getBalance().toString());
   }
 
   public void loadDepositView(InputEvent event) {
     Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
     FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxmls/DepositView.fxml"));
-    //FXMLLoader loadOverlay = new FXMLLoader(getClass().getResource("/resources/fxmls/OverlayView.fxml"));
     GridPane root = null;
-    //GridPane overlay = null;
     try { 
       root = loader.load();
-      //overlay = loadOverlay.load();
-      //DepositController controller = loader.getController();
     } catch (IOException e) {
       e.printStackTrace();
     }
-
-    //overlay.getStylesheets().add(getClass().getResource("/resources/assets/Overlay.css").toExternalForm());
-    //root.getStylesheets().add(getClass().getResource("/resources/assets/Overlay.css").toExternalForm();
-    //root.getChildren().addAll(overlay);
-    //root.getChildren().add(overlay);
     Scene scene = new Scene(root, 600, 400); 
-    scene.getStylesheets().add(getClass().getResource("/resources/assets/Overlay.css").toExternalForm());
     stage.setScene(scene);
 
   }
 
-  //@Override
-  //public void initialize(URL url, ResourceBundle rb) {
-  //@FXML
-  //private void initialize() {
-    //timer.setDaemon(true);
-    //timer.start(); 
-  //}
 }
