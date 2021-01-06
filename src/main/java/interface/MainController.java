14807e306254811108a974672c98ad236b18c7ae
Joseph Diza <josephm.diza@gmail.com>
2021-01-05 17:41:53 -0800

    Remove dead code in UI, MainController

--- src/main/java/interface/MainController.java
+++ src/main/java/interface/MainController.java
@@ -2,26 +2,7 @@ package app.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.Pane;
-
-
-//public abstract class ChangeView extends GridPane { 
-  //private ChangeView next;
-
-  //ChangeView(ChangeView next) { 
-    //this.next = next; 
-    //createView();
-  //}
-
-  //abstract void createView();
-
-  //protected void callNext() {
-      //getScene().setRoot(next);
-  //}
-
-//}
 
 public class MainController {
     @FXML
--- src/main/java/interface/UI.java
+++ src/main/java/interface/UI.java
@@ -1,13 +1,11 @@
 package app.ui;
 
 import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 public class UI extends Application {
@@ -15,27 +13,14 @@ public class UI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
       primaryStage.setTitle("My First JavaFX App");
-      //Scene scene = new Scene(((new FXMLLoader(getClass().getResource("/resources/fxmls/LoginView.fxml"))).load()), 600, 400);
-      //primaryStage.setScene(scene);
-      //primaryStage.show();
-
       primaryStage.setScene(createScene(loadMainPane()));
       primaryStage.show();
     }
 
-    //private GridPane loadMainPane() throws IOException { 
     private Pane loadMainPane() throws IOException { 
       FXMLLoader loader = new FXMLLoader(); 
-      //System.out.println(AppNavigator.LOGIN);
-      //GridPane mainPane = (GridPane) loader.load(getClass().getResourceAsStream(AppNavigator.LOGIN));
-      //GridPane mainPane = (GridPane) loader.load(getClass().getResource(AppNavigator.LOGIN));
-      //GridPane mainPane = (GridPane) loader.load(getClass().getResource(AppNavigator.LOGIN));
-      //GridPane mainPane = (GridPane) loader.load(getClass().getResourceAsStream(AppNavigator.LOGIN));
-      //GridPane mainPane = (GridPane) loader.load(getClass().getResourceAsStream(AppNavigator.MENU_BAR));
-      //GridPane mainPane = (GridPane) loader.load(getClass().getResourceAsStream(AppNavigator.MAIN));
       Pane mainPane = (Pane) loader.load(getClass().getResourceAsStream(AppNavigator.MAIN));
 
-      //MainController mainController = (MainController) loader.getController();
       MainController mainController = loader.getController();
 
       AppNavigator.setMainController(mainController);
@@ -44,10 +29,8 @@ public class UI extends Application {
       return mainPane;
     }
 
-    //private Scene createScene(GridPane mainPane) { 
     private Scene createScene(Pane mainPane) { 
       Scene scene = new Scene(mainPane); 
-
       //scene.getStylesheets().setAll(getClass().getResource("Overlay.css").toExternalForm()); 
       return scene;
     }
