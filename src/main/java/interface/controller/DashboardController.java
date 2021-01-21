576c5db03681c2135ac787c4465400dafb1ee865
Joseph Diza <josephm.diza@gmail.com>
2021-01-20 22:16:59 -0800

    Load window without overlay

--- src/main/java/interface/controller/DashboardController.java
+++ src/main/java/interface/controller/DashboardController.java
@@ -8,17 +8,13 @@ import javafx.scene.control.Label;
 import javafx.scene.input.InputEvent;
 
 public class DashboardController {
-  //@FXML OverlayController overlayController;
-  //public void setUser(BankAccount user) {
-    //overlayController.setUser(user);
-    ////this.user = user;
-    ////acct_number.setText(String.valueOf(this.user.getAcctNum()));
-    ////acct_balance.setText(this.user.getBalance().toString());
-  //} 
-  //@FXML OverlayController overlayController;
-  //@FXML OverlayController overlay;
-  
-  //public void setUser(BankAccount acct) {
-    //overlayController.setUser(acct);
-  //}
+  @FXML 
+  private OverlayController overlayController;
+
+  public void setUser(BankAccount acct) {
+    overlayController.setUser(acct);
+  }
+
+  @FXML
+  public void initialize() { }
 }
--- src/main/java/interface/controller/LoginController.java
+++ src/main/java/interface/controller/LoginController.java
@@ -19,17 +19,6 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.Parent;
 
 public class LoginController {
-  //@FXML
-  //void nextPane(ActionEvent event) { AppNavigator.loadApp(AppNavigator.DASHBOARD); }
-
-  //@FXML
-  //void nextPane(InputEvent event) { AppNavigator.loadApp(AppNavigator.DASHBOARD); }
-
-  //@FXML
-  //void nextPane(InputEvent event, BankAccount user) { 
-    //AppNavigator.loadUser(user);
-    //AppNavigator.loadApp(AppNavigator.DASHBOARD); 
-  //}
 
   @FXML
   private Button enter;
@@ -48,39 +37,19 @@ public class LoginController {
   public String getPassword() { return password.getText(); }
 
   public void processCredentials(InputEvent event) { 
-    //BankAccount user = new BankAccount(getUserName(), getPassword());
-    //nextPane(event, new BankAccount(getUserName(), getPassword()));
-    //GridPane root = null;
-    //Stage stage = null;
-    ////FXMLLoader loader = AppNavigator.getLoader(event, root, stage, AppNavigator.DASHBOARD);
-    //Object[] query = AppNavigator.getLoader(event, root, stage, AppNavigator.DASHBOARD);
-    //FXMLLoader loader = (FXMLLoader) query[0];
-    //stage = (Stage) query[1];
-    //root = (GridPane) query[2];
-    ////stage.setRoot(root);
-    ////loader.<DashboardController>getController().setUser(user); 
-    //Scene scene = ((Node)event.getSource()).getScene();
-    ////scene.setRoot((Parent)root);
-    //scene.setRoot(root);
-    //stage.setScene(scene); 
-
     Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); 
     FXMLLoader loader = new FXMLLoader(getClass().getResource(AppNavigator.DASHBOARD));
     GridPane root = null;
     try { 
       root = loader.load();
-      //loader.load();
-      //root = loader.getRoot();
     } catch (IOException e) {
       e.printStackTrace();
     }
     //loader.<DashboardController>getController().setUser(user); 
-    //Context.getInstance().currentCountry().setCountry("Belgium");
     Context.getInstance().setUser(getUserName(), getPassword());
     Scene scene = new Scene(root, 600, 400); 
     stage.setScene(scene);
-
-    //AppNavigator.setScene(stage, root);
+    stage.show();
   } 
 
   @FXML
