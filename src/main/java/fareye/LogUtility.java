37f89d5cedd679fe829435084349e1da199623b9
Joseph Diza <josephm.diza@gmail.com>
2022-03-28 23:21:07 -0700

    Add delimeters to LogUtility

--- src/main/java/fareye/LogUtility.java
+++ src/main/java/fareye/LogUtility.java
@@ -4,7 +4,11 @@ import java.lang.StringBuilder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class LogUtility implements Delims {
+public class LogUtility {
+  static final String TAB_DELIM = "\\t+";
+  static final String CARRIAGE_RETURN_DELIM = "\\r?\\n";
+  static final String NEWLINE_DELIM = "\n";
+  static final String COMMA_DELIM = ", ";
 
   /**
     * Generates a time stamp formatted as YYYY.MM.DD.HH.mm.sss
