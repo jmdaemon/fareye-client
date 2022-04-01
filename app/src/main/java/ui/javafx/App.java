736f772fc0eb82b878abdc7063762553a53596d6
Joseph Diza <josephm.diza@gmail.com>
2022-03-31 17:16:35 -0700

    Prune dead code

--- app/src/main/java/ui/javafx/App.java
+++ app/src/main/java/ui/javafx/App.java
@@ -3,7 +3,6 @@ package app.ui.javafx;
 import java.io.IOException;
 
 import javafx.application.Application;
-//import javafx.scene.Scene;
 import javafx.scene.*;
 
 import javafx.stage.Stage;
@@ -18,13 +17,10 @@ import javafx.scene.layout.AnchorPane;
 public class App extends Application {
 
   @Override
-  //public void start(Stage stage) throws Exception {
   public void start(Stage stage) {
-    //Scene scene = new Scene(((new FXMLLoader(getClass().getResource("/resources/fxmls/LoginView.fxml"))).load()), 600, 400);
     Scene scene = null;
     try {
     scene = new Scene(((new FXMLLoader(getClass().getResource("/resources/fxmls/MainWindow.fxml"))).load()), 600, 400);
-    //Scene scene = new Scene(new Group(), 600, 400);
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -32,11 +28,6 @@ public class App extends Application {
     // Load the StackPane
     StackPane stackPane = null;
     stackPane = (StackPane) scene.getRoot().getChildrenUnmodifiable().get(0);
-    //try {
-      //stackPane = (new FXMLLoader(getClass().getResource("/resources/fxmls/MainWindow.fxml"))).load();
-    //} catch (IOException e) {
-      //e.printStackTrace();
-    //}
 
     // Load our window
     AnchorPane homePane = null;
