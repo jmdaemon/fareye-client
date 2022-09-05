82ba7f5357109e92d2cb8231f72f8b0ecd25f5dc
Joseph Diza <josephm.diza@gmail.com>
2022-09-05 00:51:38 -0700

    Simplify Deposit controller & view using TransactionButton control
    The input field for deposit now only accepts numberic values. The regex
    still needs to be tightened more though to accept only one dot.

--- app/src/main/java/ui/jfx/controller/Deposit.java
+++ app/src/main/java/ui/jfx/controller/Deposit.java
@@ -1,45 +1,47 @@
 package ui.jfx.controller;
 
 // Imports
+import ui.jfx.components.TransactionButton;
 import ui.jfx.Global;
 
 // Standard Library
 import java.math.BigDecimal;
+import java.lang.NumberFormatException;
 
 // JavaFX
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-// TODO: Implement Deposit, Withdraw as variants of a custom Composite Control
 public class Deposit {
 
     // Template fields
     @FXML private AnchorPane ap_deposit;
-    @FXML private Button btn_deposit;
-    @FXML private HBox hb_deposit;
-    @FXML private TextField tf_amount;
     @FXML private VBox vb_deposit;
+    @FXML private TransactionButton tb_deposit;
 
     @FXML
     public void initialize() {
-        this.btn_deposit.setOnMouseClicked(e -> {
+        this.tb_deposit.getButton().setOnMouseClicked(e -> {
             var logger = Global.getLogger();
-            var amount = tf_amount.getText();
-            var amt = BigDecimal.valueOf(Integer.parseInt(amount));
-            logger.debug("Amount Entered: " + amt);
-
+            var amount = tb_deposit.getTextFieldLabel();
+            var amt = BigDecimal.valueOf(0);
             // TODO: Check if amount is properly formatted
-            // TODO: Add filtering to text field to automatically filter out invalid numbers
-            //Integer.parseInt(amount);
-            var acct = Global.getAcct();
-            acct.deposit(amt);
+            try {
+                amt = BigDecimal.valueOf(Integer.parseInt(amount));
+                logger.debug("Amount Entered: " + amt);
+            } catch (NumberFormatException nfe) {
+                nfe.printStackTrace();
+            }
 
-            // Since we cannot actually see the bank account updates yet, this will do for now.
-            logger.debug("Account Balance: " + Global.currencyFormat(acct.getBalance()));
+            if (amt.compareTo(BigDecimal.valueOf(0)) != 0) {
+                //Integer.parseInt(amount);
+                var acct = Global.getAcct();
+                acct.deposit(amt);
+
+                // Since we cannot actually see the bank account updates yet, this will do for now.
+                logger.debug("Account Balance: " + Global.currencyFormat(acct.getBalance()));
+            }
         });
     }
 }
