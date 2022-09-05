2e5f9962397674d49f343f47ec97f04c575aef1f
Joseph Diza <josephm.diza@gmail.com>
2022-09-04 22:46:58 -0700

    Fix compilation for fareye unit tests

--- app/src/test/java/fareye/ClientTests.java
+++ app/src/test/java/fareye/ClientTests.java
@@ -1,23 +1,23 @@
-package app.fareye;
+package fareye;
 
 // JUnit 5
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
 public class ClientTests {
-  private Client client;
-  private Client allen;
-  private Client bateman;
+  private static Client client = null;
+  private static Client paul = null;
+  private static Client bateman = null;
 
   @BeforeEach
   public void setUp() {
-    this.client = new Client("John", "M.", "Doe", "money");
-    this.paul = new Client("Paul", "Gardner", "Allen", "startup");
-    this.bateman = new Client("Patrick", "Stewart", "Bateman", "murder");
+    client = new Client("John", "M.", "Doe", "money");
+    paul = new Client("Paul", "Gardner", "Allen", "startup");
+    bateman = new Client("Patrick", "Stewart", "Bateman", "murder");
   }
 
   public static void canGenerateNonNullPassword() {
-    String result = this.client.generatePassword(32);
+    String result = client.generatePassword(32);
     assertNotNull(result, "Should generate a non null password");
     assertEquals(result.length(), "Password should be 32 characters long");
   }
