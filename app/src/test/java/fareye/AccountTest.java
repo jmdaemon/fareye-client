e7a919a2071d6c37137e2e9fbbd57bd328b1ed7f
Joseph Diza <josephm.diza@gmail.com>
2022-09-07 22:21:05 -0700

    Refactor Account tests

--- app/src/test/java/fareye/AccountTest.java
+++ app/src/test/java/fareye/AccountTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTests {
+  private static BigDecimal ZERO = BigDecimal.valueOf(0);
   private static Account acct = null;
   private static Account targ = null;
 
@@ -17,20 +18,28 @@ class AccountTests {
     targ = new Account("Magnus", "", "Chase");
   }
 
+  // Helper Functions
+  public String getLastMessage(Account account) {
+    var msgs = account.getTransactions();
+    var last = msgs.size() - 1;
+    var res  = msgs.get(last);
+    return res;
+  }
+
   @Test
   void canDepositPositiveAmount() {
     var amt = BigDecimal.valueOf(500);
     acct.deposit(amt);
     assertEquals(acct.getBalance(), amt);
-    assertEquals(acct.getTransactions().get(1), "Deposit Successful");
+    assertEquals("Deposit Successful", getLastMessage(acct));
   }
 
   @Test
   void cantDepositNegativeAmount() {
     var amt = BigDecimal.valueOf(-500);
     acct.deposit(amt);
-    assertEquals(acct.getBalance(), BigDecimal.valueOf(0));
-    assertEquals(acct.getTransactions().get(1), "Deposit Unsuccessful");
+    assertEquals(ZERO, acct.getBalance());
+    assertEquals("Deposit Unsuccessful", getLastMessage(acct));
   }
 
   @Test
@@ -43,7 +52,7 @@ class AccountTests {
     var withdrawal = BigDecimal.valueOf(50);
     acct.withdraw(withdrawal);
     assertEquals(amt.subtract(withdrawal), acct.getBalance());
-    assertEquals("Withdrawal Successful", acct.getTransactions().get(2));
+    assertEquals("Withdrawal Successful", getLastMessage(acct));
   }
 
   @Test
@@ -55,25 +64,24 @@ class AccountTests {
     // Withdraw some of this amount required
     var withdrawal = BigDecimal.valueOf(500);
     acct.withdraw(withdrawal);
-    assertEquals(acct.getBalance(), amt.subtract(withdrawal));
-    assertEquals(acct.getTransactions().get(2), "Withdrawal Successful");
+    assertEquals(amt.subtract(withdrawal), acct.getBalance());
+    assertEquals("Withdrawal Successful", getLastMessage(acct));
   }
 
   @Test
   void cantWithdrawNegativeAmount() {
     var amt = BigDecimal.valueOf(-500);
     acct.withdraw(amt);
-    assertEquals(BigDecimal.valueOf(0), acct.getBalance());
-    assertEquals("Withdrawal Unsuccessful", acct.getTransactions().get(1));
+    assertEquals(ZERO, acct.getBalance());
+    assertEquals("Withdrawal Unsuccessful", getLastMessage(acct));
   }
 
   @Test
   void cantOverdrawAmount() {
     var amt = BigDecimal.valueOf(500);
-    //acct.deposit(amt);
     acct.withdraw(amt);
-    assertEquals(acct.getBalance(), BigDecimal.valueOf(0));
-    assertEquals(acct.getTransactions().get(1), "Withdrawal Unsuccessful");
+    assertEquals(ZERO, acct.getBalance());
+    assertEquals("Withdrawal Unsuccessful", getLastMessage(acct));
   }
 
   @Test
@@ -86,12 +94,12 @@ class AccountTests {
     acct.transferTo(transferAmount, targ);
 
     // Check the balances
-    assertEquals(acct.getBalance(), expectedAmount);
-    assertEquals(targ.getBalance(), transferAmount);
+    assertEquals(expectedAmount, acct.getBalance());
+    assertEquals(transferAmount, targ.getBalance());
 
     // Check the logs
-    assertEquals(acct.getTransactions().get(3), "Transfer of $10 to account #2");
-    assertEquals(targ.getTransactions().get(2), "Transfer of $10 received from account #1");
+    assertEquals("Transfer of $10 to account #2", getLastMessage(acct));
+    assertEquals("Transfer of $10 received from account #1", getLastMessage(targ));
   }
 
   @Test
